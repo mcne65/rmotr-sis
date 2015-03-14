455aeeeab35fc893f8d31635e7ada5edcfb0ddfd
Martin Zugnoni <martin.zugnoni@gmail.com>
2015-03-14 20:48:20 -0300

    Implement persons query search

--- rmotr_sis/students/views.py
+++ rmotr_sis/students/views.py
@@ -1,5 +1,6 @@
 from __future__ import division, unicode_literals, absolute_import
 
+from django.db.models import Q
 from django.views.generic import (CreateView, UpdateView, DeleteView,
                                   ListView, FormView)
 
@@ -12,4 +13,13 @@ class PersonListView(ListView):
     paginate_by = 50
 
     def get_queryset(self):
-        return Person.objects.all()
+        query = self.request.GET.get('q')
+        if query:
+            return Person.objects.filter(
+                Q(first_name__icontains=query) |
+                Q(last_name__icontains=query) |
+                Q(lg_full_name__icontains=query) |
+                Q(email__icontains=query)
+            )
+        else:
+            return Person.objects.all()
