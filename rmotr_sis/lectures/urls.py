88a2a98f100352b9d1f5245a041a8a4856cd52b9
alan johnson <ajohnson88@gmail.com>
2015-04-23 12:34:08 -0400

    basic structure set, currently getting a 'reverse match' template error. Check lectures/urls.py

--- rmotr_sis/lectures/urls.py
+++ rmotr_sis/lectures/urls.py
@@ -3,6 +3,10 @@ from django.conf.urls import patterns, include, url
 from lectures import views
 
 urlpatterns = patterns('lectures.views',
-    url(r'^$', views.class_index, name="class_index"), 
-    url(r'^(?P<class_id>[0-9]+)$', views.class_detail, name="class_detail"),
+    #/lectures/
+    url(r'^$', views.course_index, name="course_index"),
+    #/lectures/<course_id>/
+    url(r'^(?P<course_id>[0-9]+)$', views.class_index, name="class_index"),
+    #/lectures/5/1   /lectures/<course_id>/<class_id>
+    url(r'^(?P<course_id>[0-9]+)/(?P<class_id>[0-9]+)/$', views.class_detail, name="class_detail"),
 )
--- rmotr_sis/lectures/views.py
+++ rmotr_sis/lectures/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
-from .models import Lecture
+from .models import Lecture, Course
 from django.http import Http404
 
 
-def class_index(request):
+
+def course_index(request):
     try:
-        lecture_list = Lecture.objects.order_by('pk')
-    except Lecture.DoesNotExist:
+        course_list = Course.objects.order_by("pk")
+    except Course.DoesNotExist:
+        course_list = ["No courses found"]
+    context = {'course_list':course_list}
+    return render(request, 'lectures/course_index.html', context)
+
+def class_index(request, course_id):
+    try:    
+        course = Course.objects.get(pk=course_id)
+        num_courses = len(course.lecture_set.all())
+        #Stores each lecture object in a list for access in template
+        lecture_list = [course.lecture_set.get(pk=x) for x in range(1, num_courses +1)]
+    except Course.DoesnNotExist or Lecture.DoesNotExist:
         lecture_list = ['No classes found.']
     context = {'lecture_list':lecture_list}
     return render(request, 'lectures/class_index.html', context)
