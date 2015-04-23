25b8f021c5cb94ec3aac4182b75a553a8095d36a
alan johnson <ajohnson88@gmail.com>
2015-04-23 18:16:27 -0400

    added link in class directory back to course directory, edited comments in urls.py outlining link structure.

--- rmotr_sis/lectures/urls.py
+++ rmotr_sis/lectures/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import patterns, include, url
 from lectures import views
 
 urlpatterns = patterns('lectures.views',
-    #/lectures/
+    #/courses/
     url(r'^$', views.course_index, name="course_index"),
-    #/lectures/<course_id>/
+    #/courses/<course_id>/
     url(r'^(?P<course_id>[0-9]+)$', views.class_index, name="class_index"),
-    #/lectures/5/1   /lectures/<course_id>/<class_id>
+    #/courses/<course_id>/<class_id>
     url(r'^(?P<course_id>[0-9]+)/(?P<class_id>[0-9]+)/$', views.class_detail, name="class_detail"),
 )
