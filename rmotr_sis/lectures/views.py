78f3e71fccefd281bf79349f668692100ebb9236
alan johnson <ajohnson88@gmail.com>
2015-04-27 23:08:41 -0400

    cleaned up views, removed search

--- rmotr_sis/lectures/views.py
+++ rmotr_sis/lectures/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Lecture, Course
 from django.http import Http404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.db.models import Q
 
 
@@ -11,15 +11,7 @@ class CourseListView(ListView):
     paginate_by = 20
 
     def get_queryset(self):
-        query = self.request.GET.get('q')
-        if query:
-            return Course.objects.filter(
-                Q(name__icontains=query) |
-                Q(code__icontains=query)
-            )
-        else:
-            return Course.objects.order_by("pk")
-
+        return Course.objects.order_by("pk")
 
 def class_index(request, course_id):
     try:    
@@ -35,14 +27,4 @@ def class_index(request, course_id):
 def class_detail(request, course_id, class_id): 
     lecture = get_object_or_404(Lecture, pk=class_id)
     return render(request, 'lectures/class_detail.html', {'lecture':lecture})
-                                                          
-"""
-def course_index(request):
-    try:
-        course_list = Course.objects.order_by("pk")
-    except Course.DoesNotExist:
-        course_list = None
-    context = {'course_list':course_list}
-    return render(request, 'lectures/course_index.html', context)
-"""
 
