3dfcdecd9076d97a7a4a03224629b2ee27acdbaa
alan johnson <ajohnson88@gmail.com>
2015-04-27 22:04:11 -0400

    Changed course_index view to generic listView, added search function

--- rmotr_sis/lectures/urls.py
+++ rmotr_sis/lectures/urls.py
@@ -4,9 +4,12 @@ from lectures import views
 
 urlpatterns = patterns('lectures.views',
     #/courses/
-    url(r'^$', views.course_index, name="course_index"),
+    url(r'^$', views.CourseListView.as_view(), name="course_index"),
     #/courses/<course_id>/
     url(r'^(?P<course_id>[0-9]+)$', views.class_index, name="class_index"),
     #/courses/<course_id>/<class_id>
     url(r'^(?P<course_id>[0-9]+)/(?P<class_id>[0-9]+)/$', views.class_detail, name="class_detail"),
 )
+
+
+#url(r'^$', views.course_index, name="course_index")
--- rmotr_sis/lectures/views.py
+++ rmotr_sis/lectures/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Lecture, Course
 from django.http import Http404
+from django.views.generic import ListView
+from django.db.models import Q
 
 
+class CourseListView(ListView):
+    model = Course
+    template_name = 'lectures/course_index.html'
+    paginate_by = 20
 
-def course_index(request):
-    try:
-        course_list = Course.objects.order_by("pk")
-    except Course.DoesNotExist:
-        course_list = None
-    context = {'course_list':course_list}
-    return render(request, 'lectures/course_index.html', context)
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Course.objects.filter(
+                Q(name__icontains=query) |
+                Q(code__icontains=query)
+            )
+        else:
+            return Course.objects.order_by("pk")
+
 
 def class_index(request, course_id):
     try:    
@@ -27,5 +36,13 @@ def class_detail(request, course_id, class_id):
     lecture = get_object_or_404(Lecture, pk=class_id)
     return render(request, 'lectures/class_detail.html', {'lecture':lecture})
                                                           
-
+"""
+def course_index(request):
+    try:
+        course_list = Course.objects.order_by("pk")
+    except Course.DoesNotExist:
+        course_list = None
+    context = {'course_list':course_list}
+    return render(request, 'lectures/course_index.html', context)
+"""
 
