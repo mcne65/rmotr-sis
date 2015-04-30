92c998f73ad67bf0a862e1327ba60ece91a6c778
alan johnson <ajohnson88@gmail.com>
2015-04-30 11:33:13 -0400

    refactored tests

--- rmotr_sis/courses/tests/test_models.py
+++ rmotr_sis/courses/tests/test_models.py
@@ -7,10 +7,9 @@ class CourseTests(TestCase):
 
     def test_course_was_created(self):
         """Tests whether a course object is created"""
-        course = Course(name="Advanced Python",
-                        description="Learn advanced python techniques",
-                        code="12345")
-        course.save()
+        course = Course.objects.create(name="Advanced Python",
+                                       description="Learn advanced python techniques",
+                                       code="12345")
         self.assertEqual(Course.objects.count(),1)
         self.assertEqual(course.name, "Advanced Python")
 
@@ -20,20 +19,17 @@ class LectureTests(TestCase):
 
     def test_lecture_was_created(self):
         """Tests whether a lecture object is created."""
-        course = Course(name="Advanced Python")
-        course.save()
-        lecture = Lecture(subject="Flask", course=course)
-        lecture.save()
+        course = Course.objects.create(name="Advanced Python")
+        lecture = Lecture.objects.create(subject="Flask", course=course)
         self.assertEqual(Lecture.objects.count(), 1)
 
     def test_lecture_was_created_all_parameters(self):
         """Tests creation of a lecture object with all parameters"""
         import django.utils.timezone as time 
 
-        course = Course(name="Advanced Python")
-        course.save()
+        course = Course.objects.create(name="Advanced Python")
 
-        lecture = Lecture(
+        lecture = Lecture.objects.create(
             subject="Flask",
             date=time.now(),
             notes="Notes",
@@ -41,8 +37,6 @@ class LectureTests(TestCase):
             slides_url="www.rmotr.com",
             summary = "The flask framework",
             course=course)
-
-        lecture.save()
 
         self.assertEqual(Lecture.objects.count(), 1)
         self.assertIn(lecture.subject, "Flask")
