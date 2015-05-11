4480e5105b2887e13a41a86ac7d6c6e7a2337f33
Martin Zugnoni <martin.zugnoni@gmail.com>
2015-05-11 17:19:08 -0300

    Refactor Lecture model

--- rmotr_sis/courses/models.py
+++ rmotr_sis/courses/models.py
@@ -38,33 +38,24 @@ class CourseInstance(TimeStampedModel):
 
 
 class Lecture(TimeStampedModel):
-    subject = models.CharField(max_length=150, blank=False,
-                               unique=False, null=True)
-    date = models.DateField('Class date', default=timezone.now)
-    notes = models.TextField(blank=True, null=True)
-    video_url = models.CharField(max_length=200, blank=True,
-                                 unique=False, null=True)
-    slides_url = models.CharField(max_length=200, blank=True,
-                                  unique=False, null=True)
-    summary = models.TextField(blank=True, null=True)
     course_instance = models.ForeignKey(CourseInstance)
-
-    @property
-    def lecture_handle(self):
-        if self.subject and self.date:
-            return "{0} [{1}]".format(self.subject, self.date)
-        else:
-            return self.subject
+    title = models.CharField(max_length=150)
+    date = models.DateField(default=timezone.now)
+    content = models.TextField(blank=True, null=True)
+    video_url = models.CharField(max_length=500, blank=True, null=True)
+    slides_url = models.CharField(max_length=500, blank=True, null=True)
 
     def __str__(self):
-        return self.lecture_handle
+        return self.title
 
 
 class Assignment(TimeStampedModel):
+    title = models.CharField(max_length=255)
     lecture = models.ForeignKey(Lecture)
     difficulty = models.CharField(max_length=2, choices=DIFFICULTY_CHOICES)
     source = models.TextField()
     footer = models.TextField()
+    solution = models.TextField(blank=True, null=True)
 
     tags = TaggableManager()
 
