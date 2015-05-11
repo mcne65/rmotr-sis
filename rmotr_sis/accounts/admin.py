77739cd55930c093602e78315cd7de90051679a3
Martin Zugnoni <martin.zugnoni@gmail.com>
2015-05-11 16:32:02 -0300

    Use custom admin class for User model

--- rmotr_sis/accounts/admin.py
+++ rmotr_sis/accounts/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import ugettext_lazy as _
 
 from accounts.models import User
 
-admin.site.register(User)
+
+class AccountsUserAdmin(UserAdmin):
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email',
+                                         'birth_date', 'twitter_handle',
+                                         'github_handle', 'cloud9_handle',
+                                         'timezone')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+admin.site.register(User, AccountsUserAdmin)
