4f584bcf52c2bc5369204a893693fe5b69233505
Santiago Basulto <santiago.basulto@gmail.com>
2015-06-02 09:34:16 -0300

    Added migrate task to deploy script

--- rsync-release.sh
+++ rsync-release.sh
@@ -8,6 +8,9 @@ rsync -avi --exclude '*__pycache__*' --exclude '*.pyc' rmotr_sis/ root@sis.rmotr
 echo "====== COLLECTING STATIC FILES ======"
 ssh root@sis.rmotr.com 'PYTHONPATH=/opt/rmotr-sis/rmotr_sis/ DJANGO_SETTINGS_MODULE=rmotr_sis.settings.production python /opt/rmotr-sis/rmotr_sis/manage.py collectstatic --noinput'
 
+echo "====== MIGRATING DATABASE ======"
+ssh root@sis.rmotr.com 'PYTHONPATH=/opt/rmotr-sis/rmotr_sis/ DJANGO_SETTINGS_MODULE=rmotr_sis.settings.production python /opt/rmotr-sis/rmotr_sis/manage.py migrate'
+
 echo "====== RESTARTING APP ======"
 ssh root@sis.rmotr.com 'supervisorctl restart rmotr_sis'
 
