92a1b2ba6a43b05352f8c51301b5b2d4f8d53873
Martin Zugnoni <martin.zugnoni@gmail.com>
2015-06-03 16:06:35 -0300

    Also rsync requirements

--- rsync-release.sh
+++ rsync-release.sh
@@ -3,7 +3,8 @@
 echo "Releasing..."
 
 echo "====== RSYNCING LOCAL FILES ======"
-rsync -avi --exclude '*__pycache__*' --exclude '*.pyc' rmotr_sis/ root@sis.rmotr.com:/opt/rmotr-sis/rmotr_sis
+rsync -ai --exclude '*__pycache__*' --exclude '*.pyc' rmotr_sis/ root@sis.rmotr.com:/opt/rmotr-sis/rmotr_sis
+rsync -ai requirements/ root@sis.rmotr.com:/opt/rmotr-sis/requirements
 
 echo "====== COLLECTING STATIC FILES ======"
 ssh root@sis.rmotr.com 'PYTHONPATH=/opt/rmotr-sis/rmotr_sis/ DJANGO_SETTINGS_MODULE=rmotr_sis.settings.production python /opt/rmotr-sis/rmotr_sis/manage.py collectstatic --noinput'
