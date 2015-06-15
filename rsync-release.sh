1f35ac62bdad3d0ccabe39cd84b3d110e1affce8
Santiago Basulto <santiago.basulto@gmail.com>
2015-06-15 11:36:15 -0300

    Updated sync script to rsync static files

--- rsync-release.sh
+++ rsync-release.sh
@@ -8,7 +8,8 @@ ssh root@sis.rmotr.com 'mv /root/default.backup /home/mzugnoni/rmotr_sis_$(date 
 
 echo "====== RSYNCING LOCAL FILES ======"
 rsync -ai --exclude '*__pycache__*' --exclude '*.pyc' rmotr_sis/ root@sis.rmotr.com:/opt/rmotr-sis/rmotr_sis
-rsync -ai requirements/ root@sis.rmotr.com:/opt/rmotr-sis/requirements
+rsync -ai requirements/ root@sis.rmotr.com:/opt/rmotr-sis/requirement
+rsync -ai static/ root@sis.rmotr.com:/opt/rmotr-sis/static
 
 echo "====== COLLECTING STATIC FILES ======"
 ssh root@sis.rmotr.com 'PYTHONPATH=/opt/rmotr-sis/rmotr_sis/ DJANGO_SETTINGS_MODULE=rmotr_sis.settings.production python /opt/rmotr-sis/rmotr_sis/manage.py collectstatic --noinput'
