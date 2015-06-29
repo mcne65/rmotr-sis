8196f71175c8f417c9faf844b9b6db476652746f
Santiago Basulto <santiago.basulto@gmail.com>
2015-06-29 19:34:36 -0300

    Validate repeated passwords

--- rmotr_sis/accounts/forms.py
+++ rmotr_sis/accounts/forms.py
@@ -7,37 +7,41 @@ from accounts.models import User, TIMEZONE_CHOICES
 
 class UserSignupForm(forms.ModelForm):
 
+    # auth data
     email = forms.EmailField(label='Email address')
     password = forms.CharField(max_length=128)
     repeat_password = forms.CharField(max_length=128)
 
+    # personal data
     first_name = forms.CharField(max_length=30)
     last_name = forms.CharField(max_length=30)
-
-    birth_date = forms.DateField()
-    github_handle = forms.CharField(max_length=50)
-    cloud9_handle = forms.CharField(max_length=50)
     timezone = forms.CharField(
         max_length=150, widget=forms.Select(choices=TIMEZONE_CHOICES))
+    birth_date = forms.DateField()
+
+    # accounts
+    github_handle = forms.CharField(max_length=50)
+    cloud9_handle = forms.CharField(max_length=50)
 
 
     class Meta:
         model = User
         fields = [
             # auth data
-            'username',
-            'email',
-            'password',
-            'repeat_password',
+            'username', 'email', 'password', 'repeat_password',
 
             # personal data
-            'first_name',
-            'last_name',
-            'timezone',
-            'birth_date',
+            'first_name', 'last_name', 'timezone', 'birth_date',
 
             # accounts
-            'twitter_handle',
-            'github_handle',
-            'cloud9_handle',
+            'twitter_handle', 'github_handle', 'cloud9_handle',
         ]
+
+    def clean(self):
+        cleaned_data = super(UserSignupForm, self).clean()
+
+        # validate repeated password
+        if cleaned_data['password'] != cleaned_data['repeat_password']:
+            raise forms.ValidationError('Password is not correctly repeated')
+
+        return cleaned_data
