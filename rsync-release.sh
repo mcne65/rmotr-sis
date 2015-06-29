f34f311202fecda2f3ba55df999bf1883127fe7f
Santiago Basulto <santiago.basulto@gmail.com>
2015-06-29 17:53:29 -0300

    Rsync .git folder in deploy script

--- rsync-release.sh
+++ rsync-release.sh
@@ -2,7 +2,7 @@
 
 echo "Releasing..."
 
-echo "Backing up database"
+echo "====== BACKING UP DATABASE ======"
 ssh root@sis.rmotr.com 'PYTHONPATH=/opt/rmotr-sis/rmotr_sis/ DJANGO_SETTINGS_MODULE=rmotr_sis.settings.production python /opt/rmotr-sis/rmotr_sis/manage.py dbbackup'
 ssh root@sis.rmotr.com 'mv /root/default.backup /home/mzugnoni/rmotr_sis_$(date +%F-%H:%M:%S).sql'
 
@@ -10,6 +10,7 @@ echo "====== RSYNCING LOCAL FILES ======"
 rsync -ai --exclude '*__pycache__*' --exclude '*.pyc' rmotr_sis/ root@sis.rmotr.com:/opt/rmotr-sis/rmotr_sis
 rsync -ai requirements/ root@sis.rmotr.com:/opt/rmotr-sis/requirement
 rsync -ai static/ root@sis.rmotr.com:/opt/rmotr-sis/static
+rsync -ai .git/ root@sis.rmotr.com:/opt/rmotr-sis/.git
 
 echo "====== COLLECTING STATIC FILES ======"
 ssh root@sis.rmotr.com 'PYTHONPATH=/opt/rmotr-sis/rmotr_sis/ DJANGO_SETTINGS_MODULE=rmotr_sis.settings.production python /opt/rmotr-sis/rmotr_sis/manage.py collectstatic --noinput'
