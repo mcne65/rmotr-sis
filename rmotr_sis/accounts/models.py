ac2573860215752d4d6bee64be375163d3c0c1cf
Martin Zugnoni <martin.zugnoni@gmail.com>
2015-07-02 14:57:44 -0300

    Add extra fields in User model

--- rmotr_sis/accounts/models.py
+++ rmotr_sis/accounts/models.py
@@ -4,10 +4,29 @@ import pytz
 
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.text import slugify
 
 from rmotr_sis.models import TimeStampedModel
 
 TIMEZONE_CHOICES = tuple([(tz, tz) for tz in pytz.common_timezones])
+
+GENDER_CHOICES = (
+    ('male', 'Male'),
+    ('female', 'Female'),
+)
+
+OBJECTIVE_CHOICES = tuple([(slugify(t), t) for t in ['Get a job as a programmer',
+                                                     'Start my own company',
+                                                     'Get a promotion in my current job',
+                                                     'Personal enrichment']])
+
+OCCUPATION_CHOICES = tuple([(slugify(t), t) for t in ['Studing full-time',
+                                                      'Studing part-time',
+                                                      'Unemployed and looking for job',
+                                                      'unemployed but not looking for job',
+                                                      'Self-employeed',
+                                                      'Working part-time',
+                                                      'Working full-time']])
 
 
 class User(TimeStampedModel, AbstractUser):
@@ -31,10 +50,21 @@ class User(TimeStampedModel, AbstractUser):
             # https://github.com/django/django/blob/master/django/contrib/auth/models.py#L288
     """
 
+    # personal information
     birth_date = models.DateField(blank=True, null=True)
-    twitter_handle = models.CharField(max_length=50, blank=True, null=True)
-    github_handle = models.CharField(max_length=50, blank=True, null=True)
-    cloud9_handle = models.CharField(max_length=50, blank=True, null=True)
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES,
+                              null=True, blank=True)
     timezone = models.CharField(max_length=150, choices=TIMEZONE_CHOICES,
                                 blank=True, null=True)
+    objective = models.CharField(max_length=150, choices=OBJECTIVE_CHOICES,
+                                 null=True, blank=True)
+    occupation = models.CharField(max_length=150, choices=OCCUPATION_CHOICES,
+                                  null=True, blank=True)
+
+    # accounts information
+    github_handle = models.CharField(max_length=50, blank=True, null=True)
+    cloud9_handle = models.CharField(max_length=50, blank=True, null=True)
+    twitter_handle = models.CharField(max_length=50, blank=True, null=True)
+    linkedin_profile_url = models.URLField(max_length=750, blank=True, null=True)
+
     last_activity = models.DateTimeField(blank=True, null=True)
