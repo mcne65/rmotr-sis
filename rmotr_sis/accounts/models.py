f07d0ff2383135b6df0ec51b37be6660f9d9dbd0
Martin Zugnoni <martin.zugnoni@gmail.com>
2015-07-03 14:19:19 -0300

    Add pending method to User model

--- rmotr_sis/accounts/models.py
+++ rmotr_sis/accounts/models.py
@@ -5,6 +5,7 @@ import pytz
 from django.db import models
 from django.core import validators
 from django.utils.text import slugify
+from django.core.mail import send_mail
 from django.utils import timezone as django_timezone
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin,
@@ -103,3 +104,20 @@ class User(TimeStampedModel, AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
+
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        "Returns the short name for the user."
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email], **kwargs)
