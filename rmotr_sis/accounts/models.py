15b280bca0d089c647e501531358f5954390b6c1
Martin Zugnoni <martin.zugnoni@gmail.com>
2015-07-03 11:44:49 -0300

    Inherit User model from AbstractBaseUser instead of AbstractUser

--- rmotr_sis/accounts/models.py
+++ rmotr_sis/accounts/models.py
@@ -2,9 +2,13 @@ from __future__ import division, unicode_literals, absolute_import
 
 import pytz
 
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core import validators
 from django.utils.text import slugify
+from django.utils import timezone as tz
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin,
+                                        UserManager)
 
 from rmotr_sis.models import TimeStampedModel
 
@@ -30,26 +34,49 @@ OCCUPATION_CHOICES = tuple([(slugify(t), t) for t in ['Studing full-time',
                                                       'Working full-time']])
 
 
-class User(TimeStampedModel, AbstractUser):
+class User(TimeStampedModel, AbstractBaseUser, PermissionsMixin):
 
-    """Subclass django default AbstractUser class to add custom fields
+    """Subclass django AbstractBaseUser class to add custom fields
 
-       All basic fields [1] are alredy provided by AbstractUser class, see
-       django source code in [2].
-
-       [1] inherited fields
-            # username
-            # first_name
-            # last_name
-            # email
-            # is_staff
-            # is_active
-            # date_joined
-            # last_login
-
-       [2] AbstractUser code
-            # https://github.com/django/django/blob/master/django/contrib/auth/models.py#L288
+       See django source code here:
+          https://github.com/django/django/blob/master/django/contrib/auth/base_user.py#L48
     """
+
+    # authentication fields
+    username = models.CharField(
+        _('username'),
+        max_length=30,
+        unique=True,
+        help_text=_('Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+        validators=[
+            validators.RegexValidator(
+                r'^[\w.@+-]+$',
+                _('Enter a valid username. This value may contain only '
+                  'letters, numbers ' 'and @/./+/-/_ characters.')
+            ),
+        ],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    email = models.EmailField(_('email address'), blank=True, unique=True)
+
+    # permissions fields
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_('Designates whether the user can log into this admin site.'),
+    )
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as active. '
+            'Unselect this instead of deleting accounts.'
+        ),
+    )
 
     # personal information
     birth_date = models.DateField(blank=True, null=True)
@@ -68,4 +95,10 @@ class User(TimeStampedModel, AbstractUser):
     twitter_handle = models.CharField(max_length=50, blank=True, null=True)
     linkedin_profile_url = models.URLField(max_length=750, blank=True, null=True)
 
+    date_joined = models.DateTimeField(_('date joined'), default=tz.now)
     last_activity = models.DateTimeField(blank=True, null=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
