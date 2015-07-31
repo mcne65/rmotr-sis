23c651c176a9cf382bf25ca474e25754c6346319
Santiago Basulto <santiago.basulto@gmail.com>
2015-07-31 11:06:47 -0300

    Adding link to assignment admin page for staff users

--- rmotr_sis/assignments/views.py
+++ rmotr_sis/assignments/views.py
@@ -29,6 +29,7 @@ class ResolveAssignmentView(LoginRequiredMixin, FormView):
     def get_context_data(self, **kwargs):
         context = super(ResolveAssignmentView, self).get_context_data(**kwargs)
         context['next'] = self.request.GET.get('next')
+        context['assignment'] = self.assignment
 
         context['previous_attempts'] = AssignmentAttempt.objects.filter(
             assignment=self.assignment,
