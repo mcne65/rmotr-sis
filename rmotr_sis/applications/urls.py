a043d1c0e83d29a915bee84accc21fe6c20a9852
Santiago Basulto <santiago.basulto@gmail.com>
2015-10-27 15:03:19 -0300

    Added a special view to debug the confirmation pages for the application process

--- rmotr_sis/applications/urls.py
+++ rmotr_sis/applications/urls.py
@@ -1,9 +1,12 @@
+from django.conf import settings
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 
 from applications import views
 
 
-urlpatterns = patterns('applications.views',
+urlpatterns = patterns(
+    'applications.views',
     url(r'^$', views.ApplicationStep1View.as_view(), name="application-1"),
 
     url(r'^(?P<uuid>[\w.-]+)$', views.ApplicationStep2View.as_view(), name="application-2"),
@@ -17,3 +20,11 @@ urlpatterns = patterns('applications.views',
     url(r'^(?P<uuid>[\w.-]+)/scholarship/assignments/2$', views.ApplicationStep6View.as_view(), name="application-6"),
     url(r'^(?P<uuid>[\w.-]+)/scholarship/assignments/3$', views.ApplicationStep7View.as_view(), name="application-7"),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'applications.views',
+        url(
+            r'^conirmation-step-template/(?P<step>[0-9]+)$',
+            views.ConfirmationStepView.as_view(), name="confirmation-template"),
+    )
