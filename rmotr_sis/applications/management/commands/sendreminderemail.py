c0f6574c771ddd51bbc154b66ef1cb49e9d82802
Santiago Basulto <santiago.basulto@gmail.com>
2015-11-11 10:08:40 -0300

    Added reminder email conf + some refactoring

--- rmotr_sis/applications/management/commands/sendreminderemail.py
+++ rmotr_sis/applications/management/commands/sendreminderemail.py
@@ -24,6 +24,13 @@ CONFIG = {
         'subject': "Hey *|FNAME|*, your reserved spot for the Python course is about to expire!",
         'filter': {'selected': True, 'charge_id': ''},
         'next_url': 'applications:application-checkout'
+    },
+    'scholarships-not-finished': {
+        'template_name': 'reminders/scholarships-not-finished.html',
+        'subject': "Hey *|FNAME|*, finish submitting your scholarship application. There's still time!",
+        'filter': {'selected': False, 'status__gte': 4},
+        'exclude': {'status': 7},
+        'next_url': 'applications:application-checkout'
     }
 }
 
@@ -43,24 +50,28 @@ class Command(BaseCommand):
         action = kwargs['action']
         dry_run = kwargs['dry_run']
 
-        if action not in CONFIG.keys():
+        if action not in CONFIG:
             raise CommandError('Invalid action type, please use one of: {}'
                                ''.format(', '.join(CONFIG.keys())))
 
-        applications = Application.objects.filter(**CONFIG[action]['filter'])
+        config = CONFIG[action]
+        applications = Application.objects.filter(**config['filter'])
+
+        if 'exclude' in config:
+            applications = applications.exclude(**config['exclude'])
 
         print('Sending reminder emails...')
         print()
         for app in applications:
-            next_url = reverse(CONFIG[action]['next_url'],
+            next_url = reverse(config['next_url'],
                                args=(str(app.id),))
             context = {
                 'next_url': next_url,
                 'application': app
             }
-            subject = CONFIG[action]['subject'].replace(
+            subject = config['subject'].replace(
                 '*|FNAME|*', app.first_name)
-            template = CONFIG[action]['template_name']
+            template = config['template_name']
             if dry_run:
                 # instead of sending real emails, print in stdout
                 print('Recipient: "{}"'.format(app.email))
