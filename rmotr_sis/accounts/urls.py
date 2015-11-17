58914c02e60ceabacdc223c2ec74e0009437caec
Martin Zugnoni <martin.zugnoni@gmail.com>
2015-11-17 18:49:55 -0200

    Change password template

--- rmotr_sis/accounts/urls.py
+++ rmotr_sis/accounts/urls.py
@@ -5,9 +5,19 @@ from accounts.views import UserSignupView
 
 urlpatterns = patterns('',
     url(r'login/$', 'django.contrib.auth.views.login'),
+
     url(r'logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
 
     url(r'signup/$', UserSignupView.as_view(), name='user_signup'),
-    url(r'^signup-successful/', TemplateView.as_view(
-        template_name="registration/signup-successful.html")),
+
+    url(r'signup-successful/',
+        TemplateView.as_view(template_name="registration/signup-successful.html")),
+
+    url(r'change-password$',
+        'django.contrib.auth.views.password_change',
+        name="change-password"),
+
+    url(r'change-password-done$',
+        'django.contrib.auth.views.password_change_done',
+        name="password_change_done"),
 )
