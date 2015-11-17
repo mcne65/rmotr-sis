667c2f31031afd3d54c5c570122e71b6123e6866
Martin Zugnoni <martin.zugnoni@gmail.com>
2015-11-17 20:37:57 -0200

    Use django built in views for passwords handling

--- rmotr_sis/accounts/urls.py
+++ rmotr_sis/accounts/urls.py
@@ -1,23 +1,13 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
 from django.views.generic import TemplateView
 
 from accounts.views import UserSignupView
 
 urlpatterns = patterns('',
-    url(r'login/$', 'django.contrib.auth.views.login'),
+    url('^', include('django.contrib.auth.urls')),
 
-    url(r'logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
-
+    # Sign up
     url(r'signup/$', UserSignupView.as_view(), name='user_signup'),
-
-    url(r'signup-successful/',
+    url(r'signup-successful/$',
         TemplateView.as_view(template_name="registration/signup-successful.html")),
-
-    url(r'change-password$',
-        'django.contrib.auth.views.password_change',
-        name="change-password"),
-
-    url(r'change-password-done$',
-        'django.contrib.auth.views.password_change_done',
-        name="password_change_done"),
 )
