1ff0790782ac644263aaf8f407eaf8d5f7516bf7
Martin Zugnoni <martin.zugnoni@gmail.com>
2015-11-18 21:24:39 -0200

    Create basic update profile form

--- rmotr_sis/accounts/forms.py
+++ rmotr_sis/accounts/forms.py
@@ -6,6 +6,53 @@ from accounts.models import (User, TIMEZONE_CHOICES, GENDER_CHOICES,
                              OBJECTIVE_CHOICES, OCCUPATION_CHOICES)
 
 
+class UpdateProfileForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = [
+            # personal data
+            'gender', 'timezone', 'birth_date',
+
+            # accounts
+            'github_handle', 'cloud9_handle', 'twitter_handle',
+            'linkedin_profile_url',
+
+            # extra info
+            'objective', 'occupation',
+        ]
+
+    gender = forms.CharField(
+        max_length=15, widget=forms.Select(choices=GENDER_CHOICES),
+        required=False)
+    timezone = forms.CharField(
+        max_length=150, widget=forms.Select(choices=TIMEZONE_CHOICES),
+        label='Timezone of your current location', required=False)
+    birth_date = forms.DateField(label="Birth date (MM/DD/YYYY)",
+                                 required=False)
+
+    # accounts
+    github_handle = forms.CharField(
+        max_length=50, label='Github username', required=False)
+    cloud9_handle = forms.CharField(
+        max_length=50, label='Cloud9 username', required=False)
+    twitter_handle = forms.CharField(
+        max_length=50, label='Twitter username', required=False)
+    linkedin_profile_url = forms.URLField(
+        label='URL to your LinkedIn profile',
+        help_text='At the end of the course we will offer you to add a certification in your LinkedIn profile',
+        required=False)
+
+    # extra info
+    objective = forms.CharField(
+            max_length=150, widget=forms.Select(choices=OBJECTIVE_CHOICES),
+            label='Which is your primary objective in joining this course?',
+            required=False)
+    occupation = forms.CharField(
+            max_length=150, widget=forms.Select(choices=OCCUPATION_CHOICES),
+            label='Which is your current occupation?', required=False)
+
+
 class UserSignupForm(forms.ModelForm):
 
     # auth data
--- rmotr_sis/accounts/views.py
+++ rmotr_sis/accounts/views.py
@@ -1,10 +1,26 @@
 from __future__ import division, unicode_literals, absolute_import
 
 from django.views.generic import FormView
-from braces.views import AnonymousRequiredMixin
+from django.http import HttpResponseRedirect
+from braces.views import AnonymousRequiredMixin, LoginRequiredMixin
 
-from accounts.forms import UserSignupForm
+from accounts.forms import UserSignupForm, UpdateProfileForm
 from rmotr_sis.utils import send_template_mail
+
+
+class UpdateProfileView(LoginRequiredMixin, FormView):
+    template_name = 'registration/update_profile.html'
+    form_class = UpdateProfileForm
+    success_url = '/'
+
+    def get_form(self, form_class):
+        return form_class(instance=self.request.user,
+                          **self.get_form_kwargs())
+
+    def form_valid(self, form):
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect(self.get_success_url())
 
 
 class UserSignupView(AnonymousRequiredMixin, FormView):
