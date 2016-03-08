60dcb742f19e5399f852248725de229c2cac7ec3
Martin Zugnoni <martin.zugnoni@gmail.com>
2016-03-08 14:16:24 -0300

    Use http://execute.rmotr.com/ to run assignments

--- rmotr_sis/assignments/utils.py
+++ rmotr_sis/assignments/utils.py
@@ -11,7 +11,7 @@ except ImportError:
 
 
 def run_code(code):
-    url = "https://compile-g.remoteinterview.io/compile"
+    url = "http://execute.rmotr.com/compile"
     resp = requests.post(url, data={"language": 0, "stdin": "", "code": code})
     return resp.json()
 
